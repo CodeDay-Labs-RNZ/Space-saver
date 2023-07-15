dea0b19146c7489200b63bda0a43536921081b36
RobelHailu <robel.hailu@seattlecolleges.edu>
2023-07-14 19:12:04 -0700

    basic server create

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log("Hello")
+// bring in express and dotenv for env variables
+const express = require('express');
+const dotenv = require('dotenv').config();
+// server will run on .env variable port or default port of 3000
+const port = process.env.PORT || 3000;
+
+// initialize express and start the server
+const app = express();
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
