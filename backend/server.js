c9aa1a405a17a5c89bb0dbe16ab1332501a5080e
RobelHailu <robel.hailu@seattlecolleges.edu>
2023-07-17 18:53:15 -0700

    frontend UI setup

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 // server will run on .env variable port or default port of 3000
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 // initialize express and start the server
 const app = express();
