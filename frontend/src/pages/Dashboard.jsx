348ec8ea988ac75d59af939cd282fb9ad95de1fa
Nicklas Di Pietro <37163331+Seiyaroo@users.noreply.github.com>
2023-07-18 14:40:47 -0700

    Initial Layout (Base version)

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from 'react';
 
-function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
-}
+const HomePage = () => {
+      return (
+          <div>
+                <title>Home Page</title>
 
-export default Dashboard
+                <header>
+                      {/* Navigation Bar */}
+                </header>
+
+                <section className="hero">
+                      {/* Hero Section */}
+                </section>
+
+                <section className="services">
+                      {/* Services Section */}
+                </section>
+
+                {/*<section className="features">*/}
+                {/*      /!* Features Section *!/*/}
+                {/*</section>*/}
+
+                {/*<section className="testimonials">*/}
+                {/*      /!* Testimonials Section *!/*/}
+                {/*</section>*/}
+
+                <section className="pricing">
+                      {/* Pricing Section */}
+                </section>
+
+                <section className="contact">
+                      {/* Contact Section */}
+                </section>
+
+                <footer>
+                      {/* Footer Section */}
+                </footer>
+          </div>
+      );
+};
+
+export default HomePage;
