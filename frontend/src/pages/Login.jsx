3f6603d8cb6c30ca538e21dbf3d6dd2e6f56b208
Nicklas Di Pietro <37163331+Seiyaroo@users.noreply.github.com>
2023-07-18 15:58:52 -0700

    Initialization of project to Typescript

--- frontend/src/pages/Login.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-function Login() {
-  return (
-    <div>Login</div>
-  )
-}
-
-export default Login
--- frontend/src/pages/Register.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-function Register() {
-  return (
-    <div>Register</div>
-  )
-}
-
-export default Register
