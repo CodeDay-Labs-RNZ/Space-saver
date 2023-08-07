ada6e17e75538b8a8c0eed6f95a40eb54aa9b16a
RobelHailu <robel.hailu@seattlecolleges.edu>
2023-08-07 11:27:56 -0700

    google api integration notes

---
+++ frontend/src/components/CalendarComponent.tsx
@@ -0,0 +1,7 @@
+/* this page will contian the calendar for booking availabilities */
+
+/* things to thing about:
+    - need a calendar entry component to pick a time/date for a booking 
+      - in addition, need to think about how time/day are being shown and selected 
+    - need to indicate if a time/day is free or not (available/unavailable) 
+*/
