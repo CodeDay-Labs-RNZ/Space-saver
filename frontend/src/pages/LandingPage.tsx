5d6e05c28fa3e8aff71aaa3f5d179f4201638fb0
Nicklas Di Pietro <37163331+Seiyaroo@users.noreply.github.com>
2023-08-14 09:25:15 -0700

    Update LandingPage.tsx
    Added the contact form connection and better tailwind design choices

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import '../styles/LandingPage.css';
+import {Link} from "react-router-dom";
+import ContactForm from "./Contact";
 
 const LandingPage: React.FC = () => {
     return (
@@ -8,35 +10,39 @@ const LandingPage: React.FC = () => {
                 <h1 className="text-3xl">Welcome to the Landing Page</h1>
             </header>
 
-            <section className="p-4">
-                <h2 className="text-2xl">What is AppName?</h2>
-            </section>
+            <div className="max-w-screen-xl mx-auto flex-grow"> {/* Added max width and centered content */}
+                <section className="p-4">
+                    <h2 className="text-2xl">What is AppName?</h2>
+                </section>
 
-            <section className="p-4">
-                <h2 className="text-2xl">Our services</h2>
+                <section className="p-4">
+                    <h2 className="text-2xl">Our services</h2>
 
-                <p>
-                    - Service 1
-                    <br />
-                    - Service 2
-                    <br />
-                    - Service 3
-                </p>
-            </section>
+                    <p>
+                        - Service 1
+                        <br />
+                        - Service 2
+                        <br />
+                        - Service 3
+                    </p>
+                </section>
 
-            <section className="p-4">
-                <h2 className="text-2xl">Contact Us</h2>
-                <p>Email: info@example.com</p>
-                <p>Phone: +1 123-456-7890</p>
-            </section>
+                <section className="p-4">
+                    <h2 className="text-2xl">Contact Us</h2>
+                    <p>Email: info@example.com</p>
+                    <p>Phone: +1 123-456-7890</p>
+                </section>
+            </div>
 
             <footer className="bg-blue-500 p-4 mt-auto text-white">
                 <p>&copy; {new Date().getFullYear()} Our Business</p>
-                {/* TODO: link out to another route /contact*/}
-                <p>Contact Us</p>
+                <p>
+                    <Link to="/Contact">Contact Us</Link>
+                </p>
             </footer>
         </div>
     );
 };
+
 
 export default LandingPage;
