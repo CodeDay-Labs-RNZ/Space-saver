d81d2628169f5339d8363cdba311b7312715108d
RobelHailu <robel.hailu@seattlecolleges.edu>
2023-08-15 18:17:20 -0700

    Created calendar. logging date/time to console.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './styles/App.css';
+import './styles/Calendar.css';
 import LandingPage from './components/LandingPage';
 import Login from "./pages/login";
+import Calendar from "./pages/calendar";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 
@@ -14,6 +16,7 @@ function App() {
             <Routes>
               <Route path='/' element={<LandingPage/>}/>
               <Route path='/login' element={<Login/>}/>
+              <Route path='/calendar' element={<Calendar/>}/>
             </Routes>
           </div>
         </Router>
--- frontend/src/components/CalendarComponent.tsx
+++ frontend/src/components/CalendarComponent.tsx
@@ -1,7 +1,73 @@
-/* this page will contian the calendar for booking availabilities */
+import React, { useState } from 'react'
+import ReactCalendar from 'react-calendar';
+import { add, format } from 'date-fns';
 
 /* things to thing about:
     - need a calendar entry component to pick a time/date for a booking 
       - in addition, need to think about how time/day are being shown and selected 
     - need to indicate if a time/day is free or not (available/unavailable) 
 */
+
+interface DateType {
+  justDate: Date | null;
+  dateTime: Date | null;
+}
+
+function CalendarComponent() {
+
+  /* handling date/time selection, initially set to null */
+  const [date, setDate] = useState<DateType>({
+    justDate: null,
+    dateTime: null
+  });
+
+  console.log(date.dateTime)
+  
+  /* getting available times for the selected date */
+  const getTimes = () => {
+    if (!date.justDate) {
+      return;
+    }
+
+    const { justDate } = date;
+    /* displaying opening and closing hours for a day/week 9-7 */
+    const beginning = add(justDate, {hours: 9});
+    const end = add(justDate, {hours: 17});
+    const interval = 30; // minutes
+
+    const times = [];
+    for (let i = beginning; i <= end; i = add(i, {minutes: interval})) {
+      times.push(i);
+    }
+
+    return times;
+  }
+
+  const times = getTimes();
+
+  return (
+    <div className='h-screen flex flex-col justify-center items-center'>
+      {/* when a user has only select a date, we want to show them available times */}
+      {date.justDate ? (
+        <div className='flex gap-4'>
+           {/* Show available times for selected date  */}
+           {times?.map((time, i) => (
+            <div key={`time-${i}`} className='rounded-sm bg-gray-100 p-2'>
+              <button type='button' onClick={() => setDate((prev) => ({...prev, dateTime: time}))}>
+                {format(time, 'hh:mm')}
+              </button>
+            </div>
+           ))}
+        </div>
+      ) : ( 
+        <ReactCalendar minDate={new Date()} 
+          className='calendar p-2' 
+          view='month' 
+          onClickDay={(date) => setDate((prev) => ({...prev, justDate: date}))} 
+        />
+      )}
+    </div>
+  )
+}
+
+export default CalendarComponent;
