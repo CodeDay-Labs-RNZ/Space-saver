3189601217c51761d39bd3514fbcdd5769777d34
Nicklas Di Pietro <37163331+Seiyaroo@users.noreply.github.com>
2023-09-05 19:04:53 -0700

    Update AboutUs.tsx
    Working on adjusting aboutus to be more aesthetically pleasing using tailwind. Still a WIP

--- frontend/src/pages/AboutUs.tsx
+++ frontend/src/pages/AboutUs.tsx
@@ -2,27 +2,17 @@ import React from "react";
 
 function AboutUs() {
     return (
-        <div className="relative overflow-hidden bg-cover bg-no-repeat min-h-screen flex flex-col text-white bg-[url(../public/officespace2.jpg)]">
-            <div className="absolute inset-0 bg-black opacity-50 z-[-1]"></div>
-            <div className="flex items-center justify-center h-full">
-
-                {/*<div className="flex justify-start">*/}
-                {/*    <img width={100} height={100}*/}
-                {/*         src="/logoPlaceholderImage.svg"*/}
-                {/*         className="items-center"*/}
-                {/*         alt="logo"/>*/}
-                {/*</div>*/}
-
                 <div className="bg-white p-6 rounded-lg shadow-md">
                     <h1 className="text-3xl font-bold mb-4">Meet the Team</h1>
-                    <div className="space-y-2">
+                    <div className="space-y-2 grid grid-cols-3 gap-3">
                         <h1 className="text-xl font-semibold">Zini Wang</h1>
+                        <h2>hi I am zini</h2>
                         <h1 className="text-xl font-semibold">Robel Hailu</h1>
+                        <h2></h2>
                         <h1 className="text-xl font-semibold">Nicklas Di Pietro</h1>
+                        <h2 className="">Hi I am Nick. Please reach me at my linkedin found at Linkedin.com/Nicklasdipietro/</h2>
                     </div>
                 </div>
-            </div>
-        </div>
     );
 }
 
